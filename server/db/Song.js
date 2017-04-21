a8f2ff2ceb475dd951086407c8fbab1e31cc8280
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2017-04-20 20:44:16 -0400

    song-model-#9

---
+++ server/db/Song.js
@@ -0,0 +1,25 @@
+const Sequalize = require('sequalize');
+
+const Song = db.define('song',{
+	name:{
+		type:Sequalize.STRING,
+		defaultValue: null
+	}
+	year: {
+		type:Sequalize.STRING,
+		defaultValue: null
+	}
+	duration: {
+		type:Sequalize.NUMBER,
+		defaultValue:null
+	}
+	price: {
+		type:Sequalize.NUMBER,
+		defaultValue:null
+	}
+	description: {
+		type: Sequalize.TEXT,
+		defaultValue: null
+	}
+
+})
