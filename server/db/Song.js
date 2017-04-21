aab56960701ceaa22403f0c6e01e4c11ae4163c4
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2017-04-20 21:10:27 -0400

    comas fixed hahaha

--- server/db/Song.js
+++ server/db/Song.js
@@ -5,24 +5,24 @@ const Song = conn.define('song',{
 	name:{
 		type:Sequalize.STRING,
 		defaultValue: null
-	}
+	},
 	year: {
 		type:Sequalize.STRING,
 		defaultValue: null
-	}
+	},
 	duration: {
 		type:Sequalize.NUMBER,
 		defaultValue:null
-	}
+	},
 	price: {
 		type:Sequalize.NUMBER,
 		defaultValue:null
-	}
+	},
 	description: {
 		type: Sequalize.TEXT,
 		defaultValue: null
 	}
 
-})
+});
 
 module.exports = Song;
