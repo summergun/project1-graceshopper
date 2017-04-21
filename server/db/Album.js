201a3613b32f3fd6b55a9360a68a05453ecf5e08
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2017-04-20 20:57:07 -0400

    album-model-#7

---
+++ server/db/Album.js
@@ -0,0 +1,20 @@
+const Sequalize = require('sequalize');
+const conn = require('./conn');
+
+const Album = conn.define('album',{
+	name:{
+		type:Sequalize.STRING,
+		defaultValue: null
+	}
+	year: {
+		type:Sequalize.STRING,
+		defaultValue: null
+	}
+	genre: {
+		type:Sequalize.STRING,
+		defaultValue:null
+	}
+
+})
+
+module.exports = Album;
