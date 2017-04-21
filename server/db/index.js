336b311ab32650494bbc5bbae4953b94bbd59e04
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-04-20 21:41:12 -0400

    added seed tests

--- server/db/index.js
+++ server/db/index.js
@@ -6,7 +6,7 @@ const sync = (force) => conn.sync({ force });
 
 const seed = () => sync(true)
 .then(() => Orders.bulkCreate([
-  {completedDate: Date.now(), orderPrice: 1.99, tax: 1.99 * 0.7},
+  {completedDate: Date.now(), orderPrice: 1.99, tax: 1.99 * 0.07},
   {}
 ]));
 
--- tests/db/index.js
+++ tests/db/index.js
@@ -34,5 +34,18 @@ describe('Database Objects', function(){
       expect(attributes.tax).to.be.a('object');
     });
 
+    it('Saves data as expected', (done) => {
+      db.models.Orders.findAll()
+      .then((results) => {
+        expect(results.length).to.equal(2);
+        expect(results[0].orderPrice).to.equal(1.99);
+        expect(results[0].orderPrice).to.equal(1.99 * 0.07);
+        expect(results[0].completedDate).to.be.an.instanceOf(Date);
+        expect(results[1].orderPrice).to.be.null;
+        expect(results[1].orderPrice).to.be.null;
+        expect(results[0].completedDate).to.be.null;
+      });
+    });
+
   });
 });
