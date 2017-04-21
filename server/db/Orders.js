b7f06255c2206d51dc9647d02bbcfb3f2fab4dee
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-04-20 20:51:34 -0400

    refactored code

--- server/db/Orders.js
+++ server/db/Orders.js
@@ -1,10 +1,19 @@
+// const conn = require('./conn');
+
+// const Orders = conn.define('order', {
+//   completeDate: conn.Sequelize.DATE,
+//   orderPrice: conn.Sequelize.NUMBER,
+//   tax: conn.Sequelize.INTEGER
+// });
+
+// module.exports = Orders;
+
 const conn = require('./conn');
 
-const Orders = conn.define('order', {
-  completeDate: conn.Sequelize.DATE,
-  orderPrice: conn.Sequelize.NUMBER,
+const attrs = {
+  completedDate: conn.Sequelize.Date,
+  orderPrice: conn.Sequelize.INTEGER,
   tax: conn.Sequelize.INTEGER
-});
+};
 
-module.exports = Orders;
-
+module.exports = conn.define('order', attrs);
